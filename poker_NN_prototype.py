1d62868e35f10ab97744e8e8666ec359a878b77b
Walter Peregrim <peregrim@vt.edu>
2020-11-23 19:54:03 -0500

    added to the nn

--- poker_NN_prototype.py
+++ poker_NN_prototype.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -9,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 
 
-#simple 4 layer configurable neural network 
+#simple 4 layer configurable neural network
 class PokerNN:
 
     def __init__(self, structure, features, labels, activations):
@@ -31,12 +32,31 @@ class PokerNN:
             model.add(Dense(self.outputNodes, activation=self.outputActivation))
             model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
             return model
-        
+
         estimator = KerasClassifier(build_fn=model,epochs=epochs, batch_size=batch_size, verbose=0)
         kfold = KFold(n_splits=10, shuffle=True)
         results = cross_val_score(estimator, self.features, self.labels, cv=kfold)
         print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
+    def eval(self, epochs, batch_size, X_train, X_test, y_train, y_test):
+        labels = ['Bet', 'Check', 'Fold']
+        model = Sequential()
+        model.add(Dense(self.hiddenNodes, input_dim=self.inputNodes, activation='tanh'))
+        model.add(Dense(self.hiddenNodes, activation=self.hiddenActivation))
+        model.add(Dense(self.hiddenNodes, activation=self.hiddenActivation))
+        model.add(Dense(self.outputNodes, activation=self.outputActivation))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, verbose=0)
+        test_score = model.evaluate(X_test, y_test,batch_size=batch_size)
+        #Converts call,raise to bet
+        #yhats = [list(x) for x in model.predict(X_test,batch_size=batch_size)]
+        #new_hats = [labels[np.argmax(yhat)] for yhat in yhats]
+        #new_test = [labels[np.argmax(y)] for y in y_test]
+        #new_hats = ['Bet' if h == 'Call' or h == 'Raise' else h for h in new_hats]
+        #new_test = ['Bet' if h == 'Call' or h == 'Raise' else h for h in new_test]
+        #x = np.sum([1 for i in range(len(new_test)) if new_test[i] == new_hats[i]]) / len(new_test)
+        #print(test_score,x)
+        return test_score
 
 
 '''
